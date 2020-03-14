8bdc070bbc035d358c850acf6a10c427b2e2d705
Mr.Hope <zhangbowang1998@gmail.com>
2020-03-14 21:28:59 +0800

    ci: add self-parse

---
+++ test/parser.ts
@@ -0,0 +1,12 @@
+import * as compiledParser from '../dist/parser';
+import * as fs from 'fs';
+
+// 解释器执行 hello world
+compiledParser.run("console.log('hello world')");
+
+// 自举解释器代码
+const parserCode = fs.readFileSync('./dist/parser.js', 'utf-8');
+const bootstrap = compiledParser.run(parserCode);
+
+// 自举的解释器实行 hello world
+bootstrap.run(`console.log('parser says hello world!')`);
--- test/unit/index.ts
+++ test/unit/index.ts
@@ -33,15 +33,3 @@ describe('switch test', () => {
     it
   });
 });
-
-describe('parser compiled code test', () => {
-  // 解释器执行 hello world
-  compiledParser.run("console.log('hello world')");
-
-  // 自举解释器代码
-  const parserCode = fs.readFileSync('./dist/parser.js', 'utf-8');
-  const bootstrap = compiledParser.run(parserCode);
-
-  // 自举的解释器实行 hello world
-  bootstrap.run(`console.log('parser says hello world!')`);
-});
